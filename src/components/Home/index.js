73ba53524e069541047e81eda07c9ed96b7e0083
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2023-12-06 16:38:59 +0530

    neo_dump

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom"
 import LogoTitle from "../../assets/images/logo-s.png"
+import "./index.scss"
+
 
 const Home = function (){
 
@@ -7,6 +10,11 @@ const Home = function (){
             <div className="text-zone">
                 <h1>Hi, <br/> I'm</h1>
                 <img src={LogoTitle} alt = "developer"></img>
+                    DevAshura
+                    <br />
+                    <h1>web developer </h1>
+                    <h2>Frontend Developer/Backend Dev/ Athlete</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
         </div>
     )
